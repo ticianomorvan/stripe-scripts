5d927bda3c930ebdfc05aa0bbb420800bbe16ac1
Ticiano Morvan <ticiano.morvan@gmail.com>
2024-10-02 09:17:08 -0300

    update: add script to upload coupons

--- index.js
+++ index.js
@@ -1,77 +1,115 @@
-import fs from "node:fs"
-import Stripe from "stripe"
-import { parse } from "csv"
+import fs from "node:fs";
+import { parse } from "csv";
+import Stripe from "stripe";
 
 function getStripeSecretKey() {
-  const secretKey = process.env.STRIPE_SECRET_KEY
-  
+  const secretKey = process.env.STRIPE_SECRET_KEY;
+
   if (!secretKey || secretKey.length === 0) {
-    throw new Error("ERROR: STRIPE_SECRET_KEY wasn't provided")
+    throw new Error("ERROR: STRIPE_SECRET_KEY wasn't provided");
   }
 
-  return secretKey
+  return secretKey;
 }
 
 async function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const stripe = new Stripe(getStripeSecretKey())
+const stripe = new Stripe(getStripeSecretKey());
 
 /**
-  * Remove subscriptions matching a starting pattern.
-  * Context: I had to look for customers with a "bill emoji" at the start of the string.
-  * Improvements: Implement a RegExp search, not just in the name, could be in other places.
-  */
-async function removeSubscriptions(delimiter = ",", pattern) {
-  let parser = parse({ delimiter }, async (err, data) => {
+ * Remove subscriptions matching a starting pattern.
+ * Context: I had to look for customers with a "bill emoji" at the start of the string.
+ * Improvements: Implement a RegExp search, not just in the name, could be in other places.
+ */
+async function removeSubscriptions(pattern, delimiter = ",") {
+  const parser = parse({ delimiter }, async (err, data) => {
     if (err) {
-      console.log(err)
+      console.log(err);
 
-      return
+      return;
     }
 
     // Filter the subscriptions which start with [pattern].
-    const subscriptionsId = data.slice(1).filter((s) => s[13].startsWith(pattern)).map((s) => s[0])
+    const subscriptionsId = data
+      .slice(1)
+      .filter((s) => s[13].startsWith(pattern))
+      .map((s) => s[0]);
     for (const s of subscriptionsId) {
-      console.log(s)
-      stripe.subscriptions.cancel(s)
-      await delay(100)
+      console.log(s);
+      await stripe.subscriptions.cancel(s);
+      await delay(100);
     }
-  })
-    
-  fs.createReadStream(`subscriptions.csv`).pipe(parser)
+  });
+
+  fs.createReadStream("subscriptions.csv").pipe(parser);
 }
 
 /**
-  * Remove customers matching a starting pattern.
-  * Context: I had to look for customers with a "bill emoji" at the start of the string.
-  * Improvements: Implement a RegExp search, not just in the name, could be in other places.
-  */
-async function removeCustomers(delimiter = ",", pattern) {
-  let parser = parse({ delimiter }, async (err, data) => {
+ * Remove customers matching a starting pattern.
+ * Context: I had to look for customers with a "bill emoji" at the start of the string.
+ * Improvements: Implement a RegExp search, not just in the name, could be in other places.
+ */
+async function removeCustomers(pattern, delimiter = ",") {
+  const parser = parse({ delimiter }, async (err, data) => {
     if (err) {
-      console.log(err)
+      console.log(err);
 
-      return
+      return;
     }
 
     // Get customers which names starts with [pattern]
-    const customersId = data.slice(1).filter((c) => c[3].startsWith(pattern)).map((c) => c[0])
+    const customersId = data
+      .slice(1)
+      .filter((c) => c[3].startsWith(pattern))
+      .map((c) => c[0]);
     for (const c of customersId) {
-      console.log(c)
-      stripe.customers.del(c)
-      await delay(100)
+      console.log(c);
+      await stripe.customers.del(c);
+      await delay(100);
     }
-  })
-    
-  fs.createReadStream(`unified_customers.csv`).pipe(parser)
+  });
+
+  fs.createReadStream("unified_customers.csv").pipe(parser);
 }
 
-if (parseInt(process.env.REMOVE_SUBSCRIPTIONS)) {
-  removeSubscriptions()
+/**
+ * Upload coupons from a `.csv` file.
+ * Context: We were promoting a campaing which needed promotion codes to be handled by Stripe.
+ * Improvements: Dynamic file specification, better format support.
+ */
+async function uploadCoupons(delimiter = ",") {
+  const parser = parse({ delimiter }, async (err, data) => {
+    if (err) {
+      console.log(err);
+
+      return;
+    }
+
+    const coupons = data.map((cell) => cell[0]);
+    for (const c of coupons) {
+      console.log(c);
+      await stripe.coupons.create({
+        id: c,
+        name: c,
+        percent_off: 75,
+      });
+      await delay(100);
+    }
+  });
+
+  fs.createReadStream("coupons.csv").pipe(parser);
 }
 
-if (parseInt(process.env.REMOVE_CUSTOMERS)) {
-  removeCustomers()
+if (Number.parseInt(process.env.REMOVE_SUBSCRIPTIONS)) {
+  removeSubscriptions();
+}
+
+if (Number.parseInt(process.env.REMOVE_CUSTOMERS)) {
+  removeCustomers();
+}
+
+if (Number.parseInt(process.env.UPLOAD_COUPONS)) {
+  uploadCoupons();
 }
